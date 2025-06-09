86fd6591044e157c5206cbabbcf317140bd6a8cd
kitisaks <kitisak.spnr@gmail.com>
2025-06-09 13:05:33 +0700

    Add usage parameter

--- app/main.py
+++ app/main.py
@@ -2,15 +2,19 @@ import os
 from fastapi import FastAPI, HTTPException, Header, Depends
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer
 from starlette.status import HTTP_401_UNAUTHORIZED
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Load token from environment variable
+MODEL = "intfloat/multilingual-e5-large"
 API_TOKEN = os.environ.get("EMBED_API_TOKEN")
 
 app = FastAPI()
-model = SentenceTransformer("intfloat/multilingual-e5-large")
+model = SentenceTransformer(MODEL)
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
 class TextInput(BaseModel):
     text: str
@@ -18,7 +22,9 @@ class TextInput(BaseModel):
 # Dependency to check token
 def verify_token(authorization: str = Header(...)):
     if not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Invalid auth header format")
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED, detail="Invalid auth header format"
+        )
     token = authorization.split(" ")[1]
     if token != API_TOKEN:
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Invalid token")
@@ -27,4 +33,12 @@ def verify_token(authorization: str = Header(...)):
 def get_embedding(data: TextInput, _: None = Depends(verify_token)):
     text = f"query: {data.text.strip()}"
     embedding = model.encode(text, normalize_embeddings=True).tolist()
-    return {"embedding": embedding}
+
+    # Token count
+    token_ids = tokenizer.encode(text, add_special_tokens=True)
+    total_tokens = len(token_ids)
+
+    return {
+        "embedding": embedding,
+        "usage": {"model": MODEL, "total_tokens": total_tokens},
+    }
